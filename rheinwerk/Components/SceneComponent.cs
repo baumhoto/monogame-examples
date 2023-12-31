b3e15b0f39014d71da90a605f0877e21fc0f71a2
baumhoto <baumhoto@gmail.com>
2023-12-31 11:37:24 +0100

    latest changes

--- rheinwerk/Components/SceneComponent.cs
+++ rheinwerk/Components/SceneComponent.cs
@@ -7,7 +7,8 @@ internal class SceneComponent : DrawableGameComponent
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Texture2D _starGold;
+    // private Texture2D _starGold;
+    private Texture2D _pixel;
     private Game1 _game;
     
     public SceneComponent(Game1 game) : base(game)
@@ -21,18 +22,35 @@ internal class SceneComponent : DrawableGameComponent
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
         // TODO: use this.Content to load your game content here
-        _starGold = Game.Content.Load<Texture2D>("starGold");
+        // _starGold = Game.Content.Load<Texture2D>("starGold");
+        _pixel = new Texture2D(GraphicsDevice, 1, 1);
+        _pixel.SetData<Color>(new Color[] { Color.White });
         
         base.LoadContent();
     }
 
     public override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
+
+        int width = GraphicsDevice.Viewport.Width - 20;
+        int height = GraphicsDevice.Viewport.Height - 20;
         
         // TODO: Add your drawing code here
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_starGold, _game.Simulation.Position, Color.White);
+        // _spriteBatch.Draw(_starGold, _game.Simulation.Position, Color.White);
+        _spriteBatch.Draw(_pixel, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, 10), Color.DarkGray);
+        _spriteBatch.Draw(_pixel, new Rectangle(0,  GraphicsDevice.Viewport.Height - 10,GraphicsDevice.Viewport.Width, 10), Color.DarkGray);
+        _spriteBatch.Draw(_pixel, new Rectangle(GraphicsDevice.Viewport.Width - 10,  0, 10 ,GraphicsDevice.Viewport.Height), Color.DarkGray);
+        
+        _spriteBatch.Draw(_pixel, new Rectangle((int)(_game.Simulation.BallPosition.X * width) + 10, 
+            (int)(_game.Simulation.BallPosition.Y * height) + 10, 10, 10), Color.White);
+
+        int playerRadius = (int)(_game.Simulation.PlayerSize * height) / 2;
+        int player = (int)(height * _game.Simulation.PlayerPosition) - playerRadius + 10;
+        
+        _spriteBatch.Draw(_pixel, new Rectangle(0,  player, 10 ,playerRadius * 2), Color.DarkGray);
+        
         _spriteBatch.End();
 
         base.Draw(gameTime);
--- rheinwerk/Components/SimulationComponent.cs
+++ rheinwerk/Components/SimulationComponent.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 
 namespace rheinwerk.Components;
 
 internal class SimulationComponent : GameComponent
 {
     private readonly Game1 _game;
+
+    private Vector2 _ballVelocity = new Vector2(0.3f, 0.2f);
     
-    public Vector2 Position
+    public Vector2 BallPosition
+    {
+        get;
+        private set;
+    }
+
+    public float PlayerPosition
+    {
+        get;
+        private set;
+    }
+    
+    public float PlayerSize
     {
         get;
         private set;
@@ -15,11 +30,44 @@ internal class SimulationComponent : GameComponent
     public SimulationComponent(Game1 game) : base(game)
     {
         _game = game;
+        BallPosition = new Vector2(0.3f, 0.2f);
+        PlayerPosition = 0.5f;
+        PlayerSize = 0.2f;
     }
 
     public override void Update(GameTime gameTime)
     {
-        Position += _game.Input.Direction;
+        BallPosition += _ballVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        PlayerPosition += _game.Input.Direction.Y  * (float) gameTime.ElapsedGameTime.TotalSeconds * 0.5f;
+        
+        if (BallPosition.X < 0f)
+        {
+            if (BallPosition.Y < PlayerPosition - (PlayerSize / 2)
+                || BallPosition.Y > PlayerPosition + (PlayerSize / 2))
+            {
+                throw new Exception("Player hat verloren");
+            }
+                
+            BallPosition = new Vector2(0f, BallPosition.Y);
+            _ballVelocity *= new Vector2(-1f, 1f);
+        }
+        
+        if (BallPosition.Y < 0f)
+        {
+            BallPosition = new Vector2(BallPosition.X, 0f);
+            _ballVelocity *= new Vector2(1f, -1f);
+        }
+
+        if (BallPosition.X > 1f)
+        {
+            BallPosition = new Vector2(1f, BallPosition.Y);
+            _ballVelocity *= new Vector2(-1f, 1f);
+        }
+        if (BallPosition.Y > 1f)
+        {
+            BallPosition = new Vector2(BallPosition.X, 1f);
+            _ballVelocity *= new Vector2(1f, -1f);
+        }
         base.Update(gameTime);
     }
 }
